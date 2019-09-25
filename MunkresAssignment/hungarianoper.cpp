86b3478509a0af5d85c99a4cfae5092a92148c2c
changer707 <53569122+changer707@users.noreply.github.com>
2019-09-25 17:39:25 +0800

    添加注释1
    复制矩阵、写入indices矩阵；
    linear assignment代码不知道；

--- MunkresAssignment/hungarianoper.cpp
+++ MunkresAssignment/hungarianoper.cpp
@@ -2,14 +2,18 @@
 
 Eigen::Matrix<float, -1, 2, Eigen::RowMajor> HungarianOper::Solve(const DYNAMICM &cost_matrix)
 {
+    
+    //把cost_matrix矩阵复制到matrix中
+        
     int rows = cost_matrix.rows();
     int cols = cost_matrix.cols();
-    Matrix<double> matrix(rows, cols);
+    Matrix<double> matrix(rows, cols);    //新建矩阵matrix
     for (int row = 0; row < rows; row++) {
         for (int col = 0; col < cols; col++) {
             matrix(row, col) = cost_matrix(row, col);
         }
     }
+    
     //Munkres get matrix;
     Munkres<double> m;
     m.solve(matrix);
@@ -22,7 +26,8 @@ Eigen::Matrix<float, -1, 2, Eigen::RowMajor> HungarianOper::Solve(const DYNAMICM
             if (tmp == 0) pairs.push_back(std::make_pair(row, col));
         }
     }
-    //
+    
+    //写入indices矩阵
     int count = pairs.size();
     Eigen::Matrix<float, -1, 2, Eigen::RowMajor> re(count, 2);
     for (int i = 0; i < count; i++) {
